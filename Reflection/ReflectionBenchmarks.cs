c3591854a2f0f29bfc5d1c8af4cd0a24108d45b5
Robert Haken <haken@havit.cz>
2019-02-05 19:54:55 +0100

    Reflections - BenchmarkDotNet

---
+++ Reflection/ReflectionBenchmarks.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+
+namespace Reflection
+{
+	[SimpleJob(warmupCount: 1, launchCount: 1, targetCount: 3)]
+	public class ReflectionBenchmarks
+	{
+		[Params(1000000)]
+		public int Iterations { get; set; }
+
+
+		[Benchmark]
+		public MyClass DirectAssignment()
+		{
+			for (int i = 0; i < Iterations; i++)
+			{
+				instance.MyProperty = i;
+			}
+
+			return instance;
+		}
+
+
+		[Benchmark]
+		public MyClass Reflection()
+		{
+			for (int i = 0; i < Iterations; i++)
+			{
+				var propInfo = typeof(MyClass).GetProperty("MyProperty");
+				propInfo.SetValue(instance, i, null);
+			}
+			return instance;
+		}
+
+
+		[Benchmark]
+		public MyClass ReflectionWithCachedPropertyInfo()
+		{
+			var propInfo = typeof(MyClass).GetProperty("MyProperty");
+
+			for (int i = 0; i < Iterations; i++)
+			{
+				propInfo.SetValue(instance, i, null);
+			}
+			return instance;
+		}
+
+		[Benchmark]
+		public MyClass Dynamic()
+		{
+			dynamic dyn = instance;
+			for (int i = 0; i < Iterations; i++)
+			{
+				dyn.MyProperty = i;
+			}
+			return instance;
+		}
+
+
+		private MyClass instance = new MyClass();
+
+		public class MyClass
+		{
+			public int MyProperty { get; set; }
+		}
+	}
+}
+
+//                           Method | Iterations |         Mean |        Error |     StdDev |
+//--------------------------------- |----------- |-------------:|-------------:|-----------:|
+//                 DirectAssignment |    1000000 |     273.8 us |     24.14 us |   1.323 us |
+//                       Reflection |    1000000 | 282,122.6 us |  3,730.58 us | 204.486 us |
+// ReflectionWithCachedPropertyInfo |    1000000 | 223,217.6 us | 10,899.92 us | 597.461 us |
+//                          Dynamic |    1000000 |  12,087.8 us |  1,009.71 us |  55.346 us |
--- Reflection/ReflectionProgram.cs
+++ Reflection/ReflectionProgram.cs
@@ -1,70 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
 
 namespace Reflection
 {
 	public static class ReflectionProgram
 	{
-		private const int ITERATIONS = 1000000;
-
 		public static void Main(string[] args)
 		{
-			var instance = new MyClass();
-			var sw = new Stopwatch();
-
-			// standardní nastavení
-			sw.Start();
-			for (int i = 0; i < ITERATIONS; i++)
-			{
-				instance.MyProperty = i;
-			}
-			sw.Stop();
-			Console.WriteLine($"Standardní: {sw.ElapsedTicks:n0} ticks");
-			Console.WriteLine();
-
-			// reflection s cacheovaným PropertyInfo
-			sw.Restart();
-			var propInfo = typeof(MyClass).GetProperty("MyProperty");
-			for (int i = 0; i < ITERATIONS; i++)
-			{
-				propInfo.SetValue(instance, i, null);
-			}
-			sw.Stop();
-			Console.WriteLine($"Cacheované PropInfo: {sw.ElapsedTicks:n0} ticks");
-			Console.WriteLine();
-
-			// reflection
-			sw.Restart();
-			for (int i = 0; i < ITERATIONS; i++)
-			{
-				propInfo = typeof(MyClass).GetProperty("MyProperty");
-				propInfo.SetValue(instance, i, null);
-			}
-			sw.Stop();
-			Console.WriteLine($"Vždy nové PropInfo: {sw.ElapsedTicks:n0} ticks");
-			Console.WriteLine();
-
-			// dynamic
-			sw.Restart();
-			dynamic dyn = instance;
-			for (int i = 0; i < ITERATIONS; i++)
-			{
-				dyn.MyProperty = i;
-			}
-			sw.Stop();
-			Console.WriteLine($"Dynamic: {sw.ElapsedTicks:n0} ticks");
-			Console.WriteLine();
-
-			Console.ReadKey();
-		}
-
-		public class MyClass
-		{
-			public int MyProperty { get; set; }
+			var summmary = BenchmarkRunner.Run<ReflectionBenchmarks>();
 		}
 	}
 }
